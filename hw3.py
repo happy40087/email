5657a7af4828a73bf418731b02baaab7b3137202
happy40087 <happy40087@gmail.com>
2016-06-07 15:01:14 +0800

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -25,6 +25,7 @@ def GET(url):
 def scrape(start, url_filter):
 	further_work = {start}
 	already_seen = {start}
+	
 	i=1
 	while further_work:
 		call_tuple = further_work.pop()
@@ -33,9 +34,10 @@ def scrape(start, url_filter):
 		
 		try:
 			response = requests.get(url)
-			text = response.text
-			print(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",text))
-			print('\n')
+			text = response.text 
+			print('\033[32m')
+			print(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",text))			
+			print('\033[0m\n')	
 		except urllib.error.HTTPError as e:
 			print(e.code)
 			print(e.msg)
